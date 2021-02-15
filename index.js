5eca77c6202c88db6c72e5d58f212d9ee93ff4f6
Stephen Tillman <mrtillman-0001@gmail.com>
2021-02-15 01:20:54 -0500

    upload gravatar icon to s3 bucket

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+// https://github.com/aws/aws-sdk-php/issues/72
+
 const https = require("https");
-const fs = require("fs");
 
-const fileStream = fs.createWriteStream("avatar.jpg");
+const { 
+  S3Client, 
+  PutObjectCommand
+} = require("@aws-sdk/client-s3");
 
-const imageUrl = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg";
+const imageUrl = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=450";
+//const imageUrl = "https://en.gravatar.com/userimage/150849239/d83bae7ec0e25069af6f3a006ecbf289.png?size=450";
 
-const request = https.request(imageUrl, response => {
-  response.pipe(fileStream);
-})
+https.request(imageUrl, async response => {
+  const client = new S3Client({
+    region: "us-east-1",
+  });
+  
+  const contentLength = response.headers["content-length"];
+  const contentType = response.headers["content-type"];
+  const ext = contentType.split('/').pop();
 
-request.end();
+  const key = "1613369738141"//(new Date()).getTime().toString();
+
+  const command = new PutObjectCommand({
+    Bucket: "icons.avatarbox.io",
+    ACL: "public-read",
+    Key: `u/${key}.${ext}`,
+    ContentType: contentType,
+    ContentLength: contentLength,
+    CacheControl: "max-age=0",
+    Body: response
+  })
+
+  const result = await client.send(command);
+
+  console.log(result);
+
+}).end();
